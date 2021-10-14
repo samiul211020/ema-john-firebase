5ecab31530730a3254224c20d2a1b90eac34eb6a
Samiul <samiul211020@gmail.com>
2021-10-14 11:23:18 +0600

    third-commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAEp3hZ0JYAe4fzUq58uB3Pi_zT1ZLNpJg",
+    authDomain: "ema-john-simple-d78c7.firebaseapp.com",
+    projectId: "ema-john-simple-d78c7",
+    storageBucket: "ema-john-simple-d78c7.appspot.com",
+    messagingSenderId: "92621369009",
+    appId: "1:92621369009:web:4748139eb61c31c860fc90"
+};
+
+export default firebaseConfig;
