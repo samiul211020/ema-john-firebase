da2d4af4bdae2ff1e9097cf189ad11f42fdbc607
Samiul <samiul211020@gmail.com>
2021-10-17 01:27:16 +0600

    complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+// import useFirebase from '../../hooks/useFirebase';
 import './Login.css';
 
 const Login = () => {
+    const { signInUsingGoogle } = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+    const redirect_uri = location.state?.from || '/shop';
+
+    const handleGoogleLogin = () => {
+        signInUsingGoogle()
+            .then(result => {
+                history.push(redirect_uri);
+            })
+    }
+
     return (
         <div className="login-form">
             <div>
                 <h2>Log in</h2>
-                <form onSubmit="">
+                <form>
                     <input type="email" name="" id="" placeholder="Your Email" />
                     <br />
                     <input type="password" name="" id="" />
                     <br />
                     <input type="submit" value="Submit" />
                 </form>
-                <p>new to ema-john? <Link to="/register">Create Account</Link></p>
+                <p>new to ema-john website? <Link to="/register">Create Account</Link></p>
                 <div>------or------</div>
-                <button className="btn-regular">Google Sign In</button>
+                <button
+                    className="btn-regular"
+                    onClick={handleGoogleLogin}
+                >Google Sign In</button>
             </div>
         </div>
     );
